b5fca62dc2520f4aef5c3f876d09e3b817325067
RashmiPatil13 <124230413+RashmiPatil13@users.noreply.github.com>
2023-10-20 17:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
+	while(num >=2){
+		let reminder = num % 2;
+		output=reminder + output;
+		num=parseInt(num / 2);
+	}	
+		if(num === 1){
+	  output=1+output;
+  }
+console.log(output);	
   
-}
+	
+
 
 window.decimalToBinary = decimalToBinary;
